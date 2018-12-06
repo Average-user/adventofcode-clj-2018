66f82725f0ca78e60eee4e422559f6c259283a42
Average-user <lucaspolymeris@protonmail.com>
2018-12-06 16:35:42 -0300

    MIT license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject adventofcode-clj-2018 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot adventofcode-clj-2018.core
   :target-path "target/%s"
