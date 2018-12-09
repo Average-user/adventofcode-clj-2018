e76bea96e9e2ee71fda2bdb526d686bcf1b82df0
Average-user <lucaspolymeris@protonmail.com>
2018-12-09 16:32:04 -0300

    Day09 cleanup

--- src/adventofcode_clj_2018/day09.clj
+++ src/adventofcode_clj_2018/day09.clj
@@ -4,40 +4,25 @@
             [clojure.data.finger-tree :as f]))
 
 (defn parse-input []
-  (let [input (cs/split (slurp "resources/day09.txt") #" ")]
+  (let [input (cs/split (u/input 9) #" ")]
     [(read-string (first input)) (read-string (get input 6))]))
 
-(defn splitplace [n]
-  (case n
-    0 0
-    1 1
-    2 0
-    2))
+(defn simulate-game [players marbles]
+  (-> (fn [[scores circle] [player marble]]
+        (if (zero? (rem marble 23))
+          (let [[l c cs] (f/ft-split-at circle (- (count circle) 7))]
+            [(update scores player #(+ marble c %)) (f/ft-concat cs l)])
+          (let [[l c cs] (f/ft-split-at circle 2)
+                cs'     (if (nil? c) cs (f/conjl cs c))]
+            [scores (f/conjl (f/ft-concat cs' l) marble)])))
+      (reduce [(vec (repeat players 0)) (f/counted-double-list 0) 0]
+              (map vector (cycle (range players)) (range 1 (inc marbles))))
+      (first))) 
 
-(defn game [players marbles]
-  (loop [scores (into {} (map vector (range players) (repeat 0)))
-         circle (f/counted-double-list 0)
-         player 0
-         marble 1]
-    (let [np (rem (inc player) players)]
-      (cond
-        (> marble marbles)      (reduce max (map second scores))
-        (zero? (rem marble 23)) (let [[l c cs] (f/ft-split-at circle (- (count circle) 7))]
-                                  (recur (update scores player #(+ marble c %))
-                                         (f/ft-concat cs l)
-                                         np
-                                         (inc marble)))
-        :else (let [[l c cs] (f/ft-split-at circle (splitplace (count circle)))
-                    cs'      (if (nil? c) cs (f/conjl cs c))]
-                (recur scores
-                       (f/conjl (f/ft-concat cs' l) marble)
-                       np
-                       (inc marble)))))))
-                                       
 (defn part-1 []
   (let [[players marbles] (parse-input)]
-    (game players marbles)))
+    (reduce max (simulate-game players marbles))))
 
 (defn part-2 []
   (let [[players marbles] (parse-input)]
-    (game players (* 100 marbles))))
+    (reduce max (simulate-game players (* 100 marbles)))))
